9e117bff0be626a4c26a52cfbb2cd83a90486710
Sara L <slt7@st-andrews.ac.uk>
2026-06-20 16:20:08 +0100

    edit run

--- packagerun.R
+++ packagerun.R
@@ -1,8 +1,6 @@
 library(secr)
-library(ggplot2)
-library(dplyr)
 
-#COME BACK TO LATER CODE NOT WORKING
+library(acre)
 
 #load all datasets
 data(package = "secr")
@@ -15,37 +13,14 @@ data(ovenbird)
 #inspect and plot capthist locations
 summary(ovenCH)
 
-#NOT WORKING
-capdf3 <- ovenCH$capt
-capdf3 <- as.data.frame(capdf)
-class(capdf2)
-summary(capdf)
-
-capdf %>% 
-  count(occasion) %>% 
-  ggplot(aes(x = occasion, y = n)) +
-  geom_line() +
-  geom_point() +
-  theme_minimal()
-#DPLYR AND GGPLOT NOT WORKING WITH SECR
-
-#plot(ovenCH, tracks = TRUE)
-
-
+plot(ovenCH, tracks = TRUE)
 
 #view detectors
 traps(ovenCH)
 plot(traps(ovenCH))
-ggplot()
-
-data(ovenbird)
-ovenmask
 
 #fit model
 fit <- secr.fit(ovenCH, mask = ovenmask, trace = FALSE)
-
-#FIT MULTIPLE MODELS AND DO AIC ASSESMENT
-#THEN VISUALISE
 
 #density results
 summary(fit)
